cadaac0780dc788a106f05ff3d383a5d4664e6fe
Zaid Khan <brain@zaidkhan.me>
2017-07-10 18:56:48 -0400

    Added nicer homepage using bootstrap template

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
-require('./utilities/filelist');
+//require('./utilities/filelist');
 
 app.use(app.express.static(__dirname + '/public'));
 
