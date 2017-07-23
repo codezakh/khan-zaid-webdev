9209337eb299250df730d5a2db5459103ba94d1f
Zaid Khan <brain@zaidkhan.me>
2017-07-23 19:30:33 -0400

    Added my first route! (login page)

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -3,10 +3,30 @@
  */
 
 var WebAppMaker = angular.module('WebAppMaker', ['ngRoute']);
+
+router = function($routeProvider) {
+    $routeProvider
+        .when('/',
+            {templateUrl: "views/user/login.html"}
+        )
+        .otherwise(
+            {template: 'just kill me'}
+        )
+};
+
+WebAppMaker.config(["$routeProvider", router]);
+
+
 WebAppMaker.controller('ReflectController', ['$scope', function($scope){
     $scope.reflect = function(reflectThis) {
         if (reflectThis === undefined) return "";
         return reflectThis + "|" + reflectThis.split("").reverse().join("")
     };
 }]);
+
+loginController = function($scope) {
+    console.log("Hello from loginController");
+};
+WebAppMaker.controller("loginController", ["$scope", loginController]);
+
 
