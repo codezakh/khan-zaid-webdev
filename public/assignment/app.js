645873ef82c4601c5781464311aa7724330763e7
Zaid Khan <brain@zaidkhan.me>
2017-07-23 18:49:52 -0400

    Added angular-route and included it in index.

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -2,7 +2,7 @@
  * Created by zaidkhan on 7/21/17.
  */
 
-var WebAppMaker = angular.module('WebAppMaker', []);
+var WebAppMaker = angular.module('WebAppMaker', ['ngRoute']);
 WebAppMaker.controller('ReflectController', ['$scope', function($scope){
     $scope.reflect = function(reflectThis) {
         if (reflectThis === undefined) return "";
