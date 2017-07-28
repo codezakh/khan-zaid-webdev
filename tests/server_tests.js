33734b797f0f5be77b0ec796dbdb14562eb094cd
Zaid Khan <brain@zaidkhan.me>
2017-07-28 05:51:01 -0400

    Added mocha+sinon in preparation for unit testing backend.

---
+++ tests/server_tests.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Array', function(){
+  describe('indexOf', function(){
+    it('should rub the lotion on its skin', function(){
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
