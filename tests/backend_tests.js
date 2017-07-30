e6c2065668b409a2a3b9eae542ecbf2f2ee02b83
Zaid Khan <brain@zaidkhan.me>
2017-07-30 05:23:56 -0400

    Finally got chained promises in chai to fail mocha tests

--- tests/backend_tests.js
+++ tests/backend_tests.js
@@ -2,12 +2,10 @@ var assert = require('assert');
 
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-let chaiAsPromised = require('chai-as-promised');
 let server = require('../server');
 let should = chai.should();
 
 chai.use(chaiHttp);
-chai.use(chaiAsPromised);
 
 
 describe('the /users endpoint', () => {
@@ -25,16 +23,34 @@ describe('the /users endpoint', () => {
     });
   });
 
-  it("should allow you to find users by id", () => {
+  it("should allow you to find users by id", function(done){
+    chai.request(server)
+      .post('/api/user', {
+        username: 'testusername',
+        password: 'testpassword',
+        firstName: 'goolius',
+        lastName: 'boozler'
+      }).end((error, response) => {
+      chai.expect(error).to.be.null;
+      chai.expect(response).to.have.status(220);
+      done();
+    });
+  });
+
+  it("should just kill me", function(){
     return chai.request(server)
       .post('/api/user', {
         username: 'testusername',
         password: 'testpassword',
         firstName: 'goolius',
         lastName: 'boozler'
-      })
-      .then((response) => {
-        chai.expect(response).to.have.status(300);
-      })
+      }).then((response) => {
+      return chai.request(server)
+        .get('/api/user/17')
+        .then((response) => {
+          chai.expect(response).to.have.status(220);
+        })
+    })
   });
+
 });
