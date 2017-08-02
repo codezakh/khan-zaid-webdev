df3f7ec6ce5af9cec317eaff76c3cbc5e67c6373
Zaid Khan <brain@zaidkhan.me>
2017-08-02 02:02:40 -0400

    Changed all functions on the website edit controller to use promises.

--- public/assignment/views/website/controllers/website-edit.controller.client.js
+++ public/assignment/views/website/controllers/website-edit.controller.client.js
@@ -1,27 +1,37 @@
-(function() {
-    var app = angular.module("WebAppMaker");
-    app.controller('WebsiteEditController', function (WebsiteService, $location, $routeParams, $route) {
-        var model = this;
-        model.userId = $routeParams['uid'];
-        model.websiteId = $routeParams['wid'];
-        model.websites = WebsiteService.findWebsitesByUser(model.userId);
+(function () {
+  var app = angular.module("WebAppMaker");
+  app.controller('WebsiteEditController', function (WebsiteService, $location, $routeParams, $route) {
+    var model = this;
+    model.userId = $routeParams['uid'];
+    model.websiteId = $routeParams['wid'];
+    WebsiteService.findWebsitesByUser(model.userId)
+      .then(function (response) {
+        model.websites = response.data;
+      });
 
-        var init = function(){
-            model.website = WebsiteService.findWebsiteById(model.websiteId);
-        }
+    var init = function () {
+      WebsiteService.findWebsiteById(model.websiteId)
+        .then(function(response){
+          model.website = response.data;
+        })
+    };
 
-        init()
+    init();
 
-        model.update = function(){
-            WebsiteService.updateWebsite(model.websiteId, model.website);
-            model.websiteUpdated = true;
-        }
+    model.update = function () {
+      WebsiteService.updateWebsite(model.websiteId, model.website)
+        .then(function(response){
+          model.websiteUpdated = true;
+        });
+    };
 
-        model.delete = function(){
-            WebsiteService.deleteWebsite(model.websiteId);
-            model.websites = WebsiteService.findWebsitesByUser(model.userId);
-            $location.path('/user/' + model.userId + '/website');
-        }
+    model.delete = function () {
+      WebsiteService.deleteWebsite(model.websiteId)
+        .then(function(response){
+          model.websites = WebsiteService.findWebsitesByUser(model.userId);
+          $location.path('/user/' + model.userId + '/website');
+        });
+    }
 
-    });
+  });
 })();
