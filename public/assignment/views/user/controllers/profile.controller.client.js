6ee83c9e498fd7d2543ba20117d926460b5ccdce
Zaid Khan <brain@zaidkhan.me>
2017-08-02 01:45:19 -0400

    Refactored profilecontroller to use promises and the inernal api.

--- public/assignment/views/user/controllers/profile.controller.client.js
+++ public/assignment/views/user/controllers/profile.controller.client.js
@@ -1,18 +1,41 @@
-(function() {
-    var app = angular.module("WebAppMaker");
-    app.controller('ProfileController', function (UserService, $location, $routeParams, $route) {
+(function () {
+  var app = angular.module("WebAppMaker");
+  app.controller('ProfileController', function (UserService, $location, $routeParams, $http) {
 
-        var model = this;
-        model.userId = $routeParams['uid'];
+    var model = this;
+    model.userId = $routeParams['uid'];
 
-        var init = function () {
-            model.user = UserService.findUserById(model.userId);
-        };
-        init();
 
-        model.update = function(){
-            UserService.updateUser(model.userId, model.user);
-            model.profileUpdated = true;
-        };
-    });
+    var init = function () {
+      // model.user = UserService.findUserById(model.userId);
+      UserService.findUserById(model.userId)
+        .then(function(response){
+          model.user = response.data;
+        });
+
+    };
+    init();
+
+    model.update = function () {
+      UserService.updateUser(model.userId, model.user)
+        .then(function(response){
+          model.profileUpdated = true;
+        });
+    };
+
+    model.deleteProfile = function(){
+      UserService.deleteUser(model.userId)
+        .then(function(response){
+          $location.path('/login');
+        })
+    };
+
+    model.createProfile = function(){
+      UserService.createUser(model.userId, model.user)
+        .then(function(response){
+          let user = response.data
+          $location.path('/user/' + user._id);
+        })
+    }
+  });
 })();
