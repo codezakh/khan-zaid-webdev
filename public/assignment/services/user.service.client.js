bebd8f07525fce351c80be6d71942eebc5c84964
Zaid Khan <brain@zaidkhan.me>
2017-08-02 01:31:35 -0400

    Changed userService to use api and made change to loginctornoller.

--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -9,58 +9,25 @@
         updateUser: undefined,
         createUser: undefined
       };
-      var users = [
-        {
-          _id: "123",
-          username: "alice",
-          password: "alice",
-          firstName: "Alice",
-          lastName: "Wonder"
-        },
-        {
-          _id: "234",
-          username: "bob",
-          password: "bob",
-          firstName: "Bob",
-          lastName: "Marley"
-        },
-        {
-          _id: "345",
-          username: "charly",
-          password: "charly",
-          firstName: "Charly",
-          lastName: "Garcia"
-        },
-        {
-          _id: "456",
-          username: "jannunzi",
-          password: "jannunzi",
-          firstName: "Jose",
-          lastName: "Annunzi"
-        }
-      ];
 
       api.findUserById = function (userId) {
-        return _.find(users, ['_id', userId]);
+        return $http.get(`/api/user/${userId}`);
       };
 
       api.findUserByUsername = function (username) {
-        return _.find(users, ['username', username]);
+        return $http.get(`/api/user?username=${username}`);
       };
 
       api.findUserByCredentials = function (username, password) {
-        return _.find(users, {username: username, password: password});
+        return $http.get(`/api/user?username=${username}&password=${password}`);
       };
 
       api.updateUser = function (userId, user) {
-        var user_to_modify = api.findUserById(userId);
-        return _.assign(user_to_modify, user);
+        return $http.put(`/api/user/${userId}`, user);
       };
 
       api.createUser = function(user) {
-        user._id = String(Number(_.last(users)._id) + 1);
-        users.push(user);
-        return user;
+        return $http.post('/api/user', user);
       };
 
       return api;
--- public/assignment/views/user/controllers/login.controller.client.js
+++ public/assignment/views/user/controllers/login.controller.client.js
@@ -4,12 +4,15 @@
         var model = this;
         model.submit = function(){
             var user = UserService
-                .findUserByCredentials(model.user.username, model.user.password);
-            if (user) {
-                $location.path('/user/'+user._id);
-            } else {
-                model.badCredentials = true;
-            }
+                .findUserByCredentials(model.user.username, model.user.password)
+              .then(function(response){
+                let user = response.data
+                if (user) {
+                  $location.path('/user/'+user._id);
+                } else {
+                  model.badCredentials = true;
+                }
+              });
         }
         model.register = function(){
             $location.path('/register');
