9f37ddf38467d31b7496b2b0123d8202b538656e
Zaid Khan <brain@zaidkhan.me>
2017-08-02 21:28:32 -0400

    Updated page-new controller to use promises.

--- public/assignment/views/page/controllers/page-new.controller.client.js
+++ public/assignment/views/page/controllers/page-new.controller.client.js
@@ -1,15 +1,18 @@
-(function() {
-    var app = angular.module("WebAppMaker");
-    app.controller('PageNewController', function (PageService, $location, $routeParams) {
-        var model = this;
-        model.userId = $routeParams['uid'];
-        model.websiteId = $routeParams['wid'];
+(function () {
+  var app = angular.module("WebAppMaker");
+  app.controller('PageNewController', function (PageService, $location, $routeParams) {
+    var model = this;
+    model.userId = $routeParams['uid'];
+    model.websiteId = $routeParams['wid'];
 
-        model.new = function(){
-            var newPage = PageService.createPage(model.websiteId, model.page)
-            $location.path('/user/' + model.userId + '/website/' + model.websiteId
-                + '/page/' + newPage._id);
-        }
+    model.new = function () {
+      var newPage = PageService.createPage(model.websiteId, model.page)
+        .then(function (response) {
+          let newPage = response.data;
+          $location.path('/user/' + model.userId + '/website/' + model.websiteId
+            + '/page/' + newPage._id);
+        });
+    };
 
-    });
+  });
 })();
