56d55ef5d427c62a6209e6f7e1c563622f37842f
Zaid Khan <brain@zaidkhan.me>
2017-08-02 21:26:35 -0400

    Refactored page-edit controller to use promises.

--- public/assignment/views/page/controllers/page-edit.controller.client.js
+++ public/assignment/views/page/controllers/page-edit.controller.client.js
@@ -1,27 +1,34 @@
-(function() {
-    var app = angular.module("WebAppMaker");
-    app.controller('PageEditController', function (PageService, $location, $routeParams, $route) {
-        var model = this;
-        model.userId = $routeParams['uid'];
-        model.websiteId = $routeParams['wid'];
-        model.pageId = $routeParams['pid'];
+(function () {
+  var app = angular.module("WebAppMaker");
+  app.controller('PageEditController', function (PageService, $location, $routeParams, $route) {
+    var model = this;
+    model.userId = $routeParams['uid'];
+    model.websiteId = $routeParams['wid'];
+    model.pageId = $routeParams['pid'];
 
-        var init = function(){
-            model.page = PageService.findPageById(model.pageId);
-        };
+    var init = function () {
+      PageService.findPageById(model.pageId)
+        .then(function (response) {
+          model.page = response.data;
+        })
+    };
 
-        init();
+    init();
 
-        model.update = function(){
-            PageService.updatePage(model.pageId, model.page);
-            model.pageUpdated = true;
-        };
+    model.update = function () {
+      PageService.updatePage(model.pageId, model.page)
+        .then(function (response) {
+          model.pageUpdated = true;
+        });
+    };
 
-        model.delete = function(){
-            PageService.deletePage(model.pageId);
-            $location.path('/user/' + model.userId + '/website/'
-                + model.websiteId + '/page');
-        }
+    model.delete = function () {
+      PageService.deletePage(model.pageId)
+        .then(function (response) {
+          $location.path('/user/' + model.userId + '/website/'
+            + model.websiteId + '/page');
+        });
+    };
 
-    });
+  });
 })();
