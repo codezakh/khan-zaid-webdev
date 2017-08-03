37e5d22d227c8eb9db0439f7528dabb6ac36d4a3
Zaid Khan <brain@zaidkhan.me>
2017-08-02 21:22:42 -0400

    Updated PageService to use remote API + updated page-list with promises.

--- public/assignment/services/page.service.client.js
+++ public/assignment/services/page.service.client.js
@@ -1,75 +1,37 @@
 (function () {
-    angular
-        .module("WebAppMaker")
-        .factory("PageService", ['$http', function ($http)
-        {
+  angular
+    .module("WebAppMaker")
+    .factory("PageService", ['$http', function ($http) {
 
-            var api = {
-                createPage: undefined,
-                findPageByWebsiteId: undefined,
-                findPageById: undefined,
-                updatePage:undefined,
-                deletePage: undefined
-            };
+      var api = {
+        createPage: undefined,
+        findPageByWebsiteId: undefined,
+        findPageById: undefined,
+        updatePage: undefined,
+        deletePage: undefined
+      };
 
-            var pages = [
-                {
-                    "_id": "321",
-                    "name": "Post 1",
-                    "websiteId": "456",
-                    "description": "Lorem"
-                },
-                {
-                    "_id": "432",
-                    "name": "Post 2",
-                    "websiteId": "456",
-                    "description": "Lorem"
-                },
-                {
-                    "_id": "543",
-                    "name": "Post 3",
-                    "websiteId": "456",
-                    "description": "Lorem"
-                },
-                {
-                    "_id": "1",
-                    "name": "Henlo",
-                    "websiteId": "890",
-                    "description": "I am a description."
-                }
-            ];
+      api.findPageById = function (pageId) {
+        return $http.get(`/api/page/${pageId}`)
+      };
 
-            api.findPageById = function(pageId){
-                return _.find(pages, ['_id', pageId]);
-            };
+      api.createPage = function (websiteId, page) {
+        return $http.post(`/api/website/${websiteId}/page`, page);
+      };
 
-            api.createPage = function(websiteId, page){
-                _.assign(page, {
-                    websiteId: websiteId,
-                    _id: "hardcoded id for now"
-                });
+      api.findPageByWebsiteId = function (websiteId) {
+        return $http.get(`/api/website/${websiteId}/page`);
+      };
 
-                pages.push(page);
+      api.updatePage = function (pageId, page) {
+        return $http.put(`/api/page/${pageId}`, page);
+      };
 
-                return page
-            };
+      api.deletePage = function (pageId) {
+        $http.delete(`/api/page/${pageId}`);
+      };
 
-            api.findPageByWebsiteId = function(websiteId){
-                return _.filter(pages, ['websiteId', websiteId]);
-            };
-
-            api.updatePage = function(pageId, page){
-                page_to_update = api.findPageById(pageId);
-                _.assign(page_to_update, page);
-                return page_to_update;
-            };
-
-            api.deletePage = function(pageId){
-                var pageIdx = _.findIndex(pages, ['_id', pageId]);
-                pages.splice(pageIdx, 1);
-            };
-
-            return api;
-        }
-        ]);
+      return api;
+    }
+    ]);
 })();
--- public/assignment/views/page/controllers/page-list.controller.client.js
+++ public/assignment/views/page/controllers/page-list.controller.client.js
@@ -1,14 +1,17 @@
-(function() {
-    var app = angular.module("WebAppMaker");
-    app.controller('PageListController', function (PageService, $location, $routeParams) {
-        var model = this;
-        model.userId = $routeParams['uid'];
-        model.websiteId = $routeParams['wid'];
-        model.pages = PageService.findPageByWebsiteId(model.websiteId);
+(function () {
+  var app = angular.module("WebAppMaker");
+  app.controller('PageListController', function (PageService, $location, $routeParams) {
+    var model = this;
+    model.userId = $routeParams['uid'];
+    model.websiteId = $routeParams['wid'];
+    PageService.findPageByWebsiteId(model.websiteId)
+      .then(function (response) {
+        model.pages = response.data;
         model.noPagesWarning = false;
-        if (model.pages.length == 0){
-           model.noPagesWarning = true;
+        if (model.pages.length == 0) {
+          model.noPagesWarning = true;
         }
+      });
 
-    });
+  });
 })();
