a4fb5a8c5a9245a482278ff0a683b8aeec3c6018
Zaid Khan <brain@zaidkhan.me>
2017-08-02 21:15:52 -0400

    Fixed the website-new controller to use promises from the service.

--- public/assignment/views/website/controllers/website-new.controller.client.js
+++ public/assignment/views/website/controllers/website-new.controller.client.js
@@ -1,14 +1,20 @@
-(function() {
-    var app = angular.module("WebAppMaker");
-    app.controller('WebsiteNewController', function (WebsiteService, $location, $routeParams) {
-        var model = this;
-        model.userId = $routeParams['uid'];
-        model.websites = WebsiteService.findWebsitesByUser(model.userId);
+(function () {
+  var app = angular.module("WebAppMaker");
+  app.controller('WebsiteNewController', function (WebsiteService, $location, $routeParams) {
+    var model = this;
+    model.userId = $routeParams['uid'];
+    WebsiteService.findWebsitesByUser(model.userId)
+      .then(function (response) {
+        model.websites = response.data;
+      });
 
-        model.new = function(){
-            var newsite = WebsiteService.createWebsite(model.userId, model.website);
-            $location.path('/user/' + model.userId + '/website/' + newsite._id);
-        }
+    model.new = function () {
+      var newsite = WebsiteService.createWebsite(model.userId, model.website)
+        .then(function(response){
+          let newsite = response.data;
+          $location.path('/user/' + model.userId + '/website/' + newsite._id);
+        })
+    };
 
-    });
+  });
 })();
