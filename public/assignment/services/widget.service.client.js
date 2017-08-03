9fa5a3495f531c70dba88879127f9032da97d932
Zaid Khan <brain@zaidkhan.me>
2017-08-02 21:37:58 -0400

    Refactored widgetservice to use the remote api.

--- public/assignment/services/widget.service.client.js
+++ public/assignment/services/widget.service.client.js
@@ -12,79 +12,25 @@
                 deleteWidget: undefined
             };
 
-            var widgets = [
-                {
-                    "_id": "123",
-                    "widgetType": "HEADING",
-                    "pageId": "321",
-                    "size": 2,
-                    "text": "GIZMODO"
-                },
-                {
-                    "_id": "234",
-                    "widgetType": "HEADING",
-                    "pageId": "321",
-                    "size": 4,
-                    "text": "Lorem ipsum"
-                },
-                {
-                    "_id": "345",
-                    "widgetType": "IMAGE",
-                    "pageId": "321",
-                    "width": "100%",
-                    "url": "http://lorempixel.com/400/200/"
-                },
-                {
-                    "_id": "456",
-                    "widgetType": "HTML",
-                    "pageId": "321",
-                    "text": "<p>Lorem ipsum</p>"
-                },
-                {
-                    "_id": "567",
-                    "widgetType": "HEADING",
-                    "pageId": "321",
-                    "size": 4,
-                    "text": "Lorem ipsum"
-                },
-                {
-                    "_id": "678",
-                    "widgetType": "YOUTUBE",
-                    "pageId": "321",
-                    "width": "100%",
-                    "url": "https://youtu.be/AM2Ivdi9c4E"
-                },
-                {
-                    "_id": "789",
-                    "widgetType": "HTML",
-                    "pageId": "321",
-                    "text": "<p>Lorem ipsum</p>"
-                }
-            ];
 
             api.findWidgetById = function(widgetId){
-                return _.find(widgets, ['_id', widgetId]);
+                return $http.get(`/api/widget/${widgetId}`);
             };
 
             api.createWidget = function(pageId, widget){
-                _.assign(widget, {pageId: pageId, _id: "placeholder id"});
-                widgets.push(widget);
-                return widget;
+              return $http.post(`/api/page/${pageId}/widget`, widget);
             };
 
             api.findWidgetsByPageId = function(pageId){
-                return _.filter(widgets, ['pageId', pageId]);
+              return $http.get(`/api/page/${pageId}/widget`);
             };
 
             api.updateWidget = function(widgetId, widget){
-                var widget_to_update = api.findWidgetById(widgetId);
-                _.assign(widget_to_update, widget);
-                return widget_to_update;
+              return $http.put(`/api/widget/${widgetId}`, widget);
             };
 
             api.deleteWidget = function(widgetId){
-                var widgetIdx = _.findIndex(widgets, ['_id', widgetId]);
-                widgets.splice(widgetIdx, 1);
+              return $http.delete(`/api/widget/${widgetId}`);
             };
 
             return api;
